0892910c614a99b372112f8ad3e22a4e8fe01828
Simon Parra <simon.pahe@gmail.com>
2019-11-15 22:39:15 +0000

    adding strtok to getline

--- read_line.c
+++ read_line.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+
 /**
  * main - PID
  *
@@ -11,12 +13,23 @@ int main()
 	size_t size = 0;
 	ssize_t len = 0;
 	char *line = NULL;
+	char *piece = NULL;
 
 	while (len >= 0)
 	{
 		printf("$ ");
 		len = (getline(&line, &size, stdin));
+
+		piece = strtok(line, " ");
+
+		while (piece)
+		{
+			printf("%s", piece);
+			piece = strtok(NULL, " ");
+		}
+
 		printf("%s", line);
+
 
 
 	/*When argv[1] == "exit"; perror and return -1*/
