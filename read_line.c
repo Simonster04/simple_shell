3f5057be25a88ddb22d308a39225a5a2c4e7a27f
towasme <towasm@gmail.com>
2019-11-15 20:24:09 +0000

    getline funct

---
+++ read_line.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+/**
+ * main - PID
+ *
+ * Return: Always 0.
+ */
+int main()
+{
+	size_t size = 0;
+	ssize_t len = 0;
+	char *line = NULL;
+
+	while (len >= 0)
+	{
+		printf("$ ");
+		len = (getline(&line, &size, stdin));
+		printf("%s", line);
+	}
+	return (0);
+}
