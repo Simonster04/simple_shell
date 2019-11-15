197c5e3f3ed3eb2380430aadd21e524125dee06e
towasme <towasm@gmail.com>
2019-11-15 23:29:47 +0000

    func tokenize

---
+++ tokenize.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+
+
+
+/**
+ * main - PID
+ *
+ * Return: Always 0.
+ */
+int main(/*int ac, char **av*/)
+{
+	size_t size = 0;
+	ssize_t len = 0;
+	char *line = NULL, *piece = NULL;
+
+	while (len >= 0)
+	{
+		printf("$ ");
+		len = (getline(&line, &size, stdin));
+		piece = strtok(line, " ");
+		while (piece)
+		{
+			printf("%s -", piece);
+			piece = strtok(NULL, " ");
+		}
+	}
+	return (0);
+}
+
+
